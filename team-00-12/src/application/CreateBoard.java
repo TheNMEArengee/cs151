1f5ebc48a90b48a2210622a2a8dc11341248a3f2
TheNMEArengee <jonsay157@gmail.com>
2020-11-22 21:28:02 -0800

    moved createBoard to anotehr class

---
+++ team-00-12/src/application/CreateBoard.java
@@ -0,0 +1,68 @@
+package application;
+
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
+
+public class CreateBoard {
+	public static Parent createBoard() {
+		Group tilesGroup = new Group();
+		
+		
+		//Chess board container
+		GridPane board = new GridPane();
+
+		Rectangle[][] placementBoard = new Rectangle[Main.WIDTH][Main.HEIGHT];
+		
+		board.setPrefSize((Main.WIDTH * Main.TILE_SIZE) , (Main.HEIGHT * Main.TILE_SIZE) );
+		board.getChildren().addAll(tilesGroup);
+		
+		
+		for(int y=2; y<10; y++) {
+			for(int x=2; x<10; x++) {
+				final int coordX = x-2;
+				final int coordY = y-2;
+//				System.out.println(coordX + ", "+ coordY);
+				placementBoard[x][y] = new Rectangle();
+				placementBoard[x][y].setWidth(Main.TILE_SIZE);
+				placementBoard[x][y].setHeight(Main.TILE_SIZE);
+				placementBoard[x][y].setStroke(Color.TRANSPARENT);
+				if((x+y)%2 == 0) {
+					placementBoard[x][y].setFill(Color.BLACK);
+				}
+				else {
+					placementBoard[x][y].setFill(Color.WHITE);
+				}
+				placementBoard[x][y].setStrokeType(StrokeType.INSIDE);
+				placementBoard[x][y].setStrokeWidth(1);
+				placementBoard[x][y].relocate(x * Main.TILE_SIZE, y * Main.TILE_SIZE);
+				placementBoard[x][y].addEventHandler(MouseEvent.MOUSE_CLICKED, e->{
+					System.out.println("("+ coordX +", "+ coordY+")");
+				});
+				tilesGroup.getChildren().add(placementBoard[x][y]);
+			}
+		}	
+
+		
+		//Create an 8 x 8 chess board and store it into tilesGroup
+		for (int y = 0; y < Main.HEIGHT; y++) {
+			for (int x = 0; x < Main.WIDTH; x++) {
+				Tile tile = new Tile((x + y) % 2 == 0, x, y);
+				tile.addEventHandler(MouseEvent.MOUSE_CLICKED, e->{
+					System.out.println("Hi: " + e);
+				});
+//				tileGrid[x][y] = tile;
+//				tilesGroup.getChildren().add(tile);
+				board.setAlignment(Pos.CENTER);
+			}
+		}
+		
+		return board;
+	}
+
+}
--- team-00-12/src/application/Main.java
+++ team-00-12/src/application/Main.java
@@ -92,7 +92,7 @@ public class Main extends Application {
 			// now)
 			startBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
 				VBox battleField = constructVBox();
-				battleField.getChildren().addAll(createBoard(), backBtn);
+				battleField.getChildren().addAll(CreateBoard.createBoard(), backBtn);
 				BorderPane gameRoot = new BorderPane();
 
 				gameRoot.setCenter(battleField);
@@ -209,60 +209,60 @@ public class Main extends Application {
 	
 	
 	// Constructs the checker board (WIP)
-	private Parent createBoard() {
-		Group tilesGroup = new Group();
-		
-		
-		//Chess board container
-		GridPane board = new GridPane();
-
-		placementBoard = new Rectangle[WIDTH][HEIGHT];
-		
-		board.setPrefSize((WIDTH * TILE_SIZE) , (HEIGHT * TILE_SIZE) );
-		board.getChildren().addAll(tilesGroup);
-		
-		
-		for(int y=2; y<10; y++) {
-			for(int x=2; x<10; x++) {
-				final int coordX = x-2;
-				final int coordY = y-2;
-//				System.out.println(coordX + ", "+ coordY);
-				placementBoard[x][y] = new Rectangle();
-				placementBoard[x][y].setWidth(TILE_SIZE);
-				placementBoard[x][y].setHeight(TILE_SIZE);
-				placementBoard[x][y].setStroke(Color.TRANSPARENT);
-				if((x+y)%2 == 0) {
-					placementBoard[x][y].setFill(Color.BLACK);
-				}
-				else {
-					placementBoard[x][y].setFill(Color.WHITE);
-				}
-				placementBoard[x][y].setStrokeType(StrokeType.INSIDE);
-				placementBoard[x][y].setStrokeWidth(1);
-				placementBoard[x][y].relocate(x * TILE_SIZE, y * TILE_SIZE);
-				placementBoard[x][y].addEventHandler(MouseEvent.MOUSE_CLICKED, e->{
-					System.out.println("("+ coordX +", "+ coordY+")");
-				});
-				tilesGroup.getChildren().add(placementBoard[x][y]);
-			}
-		}	
-
-		
-		//Create an 8 x 8 chess board and store it into tilesGroup
-		for (int y = 0; y < HEIGHT; y++) {
-			for (int x = 0; x < WIDTH; x++) {
-				Tile tile = new Tile((x + y) % 2 == 0, x, y);
-				tile.addEventHandler(MouseEvent.MOUSE_CLICKED, e->{
-					System.out.println("Hi: " + e);
-				});
-//				tileGrid[x][y] = tile;
-//				tilesGroup.getChildren().add(tile);
-				board.setAlignment(Pos.CENTER);
-			}
-		}
-		
-		return board;
-	}
+//	private Parent createBoard() {
+//		Group tilesGroup = new Group();
+//		
+//		
+//		//Chess board container
+//		GridPane board = new GridPane();
+//
+//		placementBoard = new Rectangle[WIDTH][HEIGHT];
+//		
+//		board.setPrefSize((WIDTH * TILE_SIZE) , (HEIGHT * TILE_SIZE) );
+//		board.getChildren().addAll(tilesGroup);
+//		
+//		
+//		for(int y=2; y<10; y++) {
+//			for(int x=2; x<10; x++) {
+//				final int coordX = x-2;
+//				final int coordY = y-2;
+////				System.out.println(coordX + ", "+ coordY);
+//				placementBoard[x][y] = new Rectangle();
+//				placementBoard[x][y].setWidth(TILE_SIZE);
+//				placementBoard[x][y].setHeight(TILE_SIZE);
+//				placementBoard[x][y].setStroke(Color.TRANSPARENT);
+//				if((x+y)%2 == 0) {
+//					placementBoard[x][y].setFill(Color.BLACK);
+//				}
+//				else {
+//					placementBoard[x][y].setFill(Color.WHITE);
+//				}
+//				placementBoard[x][y].setStrokeType(StrokeType.INSIDE);
+//				placementBoard[x][y].setStrokeWidth(1);
+//				placementBoard[x][y].relocate(x * TILE_SIZE, y * TILE_SIZE);
+//				placementBoard[x][y].addEventHandler(MouseEvent.MOUSE_CLICKED, e->{
+//					System.out.println("("+ coordX +", "+ coordY+")");
+//				});
+//				tilesGroup.getChildren().add(placementBoard[x][y]);
+//			}
+//		}	
+//
+//		
+//		//Create an 8 x 8 chess board and store it into tilesGroup
+//		for (int y = 0; y < HEIGHT; y++) {
+//			for (int x = 0; x < WIDTH; x++) {
+//				Tile tile = new Tile((x + y) % 2 == 0, x, y);
+//				tile.addEventHandler(MouseEvent.MOUSE_CLICKED, e->{
+//					System.out.println("Hi: " + e);
+//				});
+////				tileGrid[x][y] = tile;
+////				tilesGroup.getChildren().add(tile);
+//				board.setAlignment(Pos.CENTER);
+//			}
+//		}
+//		
+//		return board;
+//	}
 	
 	
 	public void goToGame(ActionEvent event) throws IOException {
