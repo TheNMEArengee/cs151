6cd2b29784edb79093d7512c7e5fb61548c1acdd
TheNMEArengee <jonsay157@gmail.com>
2020-12-02 22:29:42 -0800

    made queens spawn

--- team-00-12/src/application/GameBoard/CheckerboardPane.java
+++ team-00-12/src/application/GameBoard/CheckerboardPane.java
@@ -48,7 +48,7 @@ public class CheckerboardPane extends Pane {
 		this.deck = new Deck();
 		setUnits();
 		setCards();
-		canvas = new Canvas(800,480);
+		canvas = new Canvas(800, 480);
 		gc = canvas.getGraphicsContext2D();
 		draw();
 	}
@@ -61,8 +61,7 @@ public class CheckerboardPane extends Pane {
 			for (int x = 0; x < 8; x++) {
 				if (x == 4 && y == 0) { // Add king
 					units.add(new Unit(x, y, player, kingPawn));
-				}
-				else { //Add pawn
+				} else { // Add pawn
 					units.add(new Unit(x, y, player, 0));
 				}
 			}
@@ -74,8 +73,7 @@ public class CheckerboardPane extends Pane {
 			for (int x = 0; x < 8; x++) {
 				if (x == 4 && y == 7) { // Add king
 					units.add(new Unit(x, y, player, kingPawn));
-				}
-				else { //Add pawn
+				} else { // Add pawn
 					units.add(new Unit(x, y, player, 0));
 				}
 			}
@@ -91,17 +89,17 @@ public class CheckerboardPane extends Pane {
 
 		// Cards in player 0 hand
 		for (int numOfCards = 0; numOfCards < 5; numOfCards++) {
-			hand0.add(new Card(Affiliation.WHITE, rand.nextInt(4)+1));
+			hand0.add(new Card(Affiliation.WHITE, rand.nextInt(5) + 1));
 		}
 
 		// Cards in player 1 hand
 		for (int numOfCards = 0; numOfCards < 5; numOfCards++) {
-			hand1.add(new Card(Affiliation.BLACK, rand.nextInt(4)+1));
+			hand1.add(new Card(Affiliation.BLACK, rand.nextInt(5) + 1));
 		}
 
 		// Cards in deck
 		for (int numOfCards = 0; numOfCards < 21; numOfCards++) {
-			deck.add(new Card(Affiliation.DECK, rand.nextInt(4)+1));
+			deck.add(new Card(Affiliation.DECK, rand.nextInt(5) + 1));
 		}
 
 		player0Hand.addAll(hand0);
@@ -136,7 +134,6 @@ public class CheckerboardPane extends Pane {
 					r.setFill(Color.rgb(125, 135, 150));
 				}
 
-
 				tileGroup.getChildren().add(r);
 			}
 		}
@@ -158,8 +155,8 @@ public class CheckerboardPane extends Pane {
 			Rectangle r = new Rectangle(x, y, 40, 40);
 
 			// Rounded edges for units
-//			r.setArcWidth(20);
-//			r.setArcHeight(20);
+			//			r.setArcWidth(20);
+			//			r.setArcHeight(20);
 
 			// Determine color of pieces, check which player the unit belongs to
 			if (u.getPlayer() == 0) {
@@ -193,13 +190,12 @@ public class CheckerboardPane extends Pane {
 		drawDeck();
 	}
 
-	//Draws player 0 hand
+	// Draws player 0 hand
 	public void drawPlayer0Hand() {
 		// Draw player 0 hand
 		int tileSize = checkerboard.getTileSize();
 		int cardSizeX = checkerboard.getCardSizeX();
 		int cardSizeY = checkerboard.getCardSizeY();
-
 
 		for (Card c : player0Hand.getHand()) {
 			int handPosition = player0Hand.getHand().indexOf(c);
@@ -217,22 +213,21 @@ public class CheckerboardPane extends Pane {
 			r.setStroke(Color.BLACK);
 			tileGroup.getChildren().add(r);
 
-			//Generating card text for player 0 cards
+			// Generating card text for player 0 cards
 			Text t = new Text(x + 10, y + 20, c.getTitle() + "");
 			tileGroup.getChildren().add(t);
 
-			//Generating images for player 1 cards
+			// Generating images for player 1 cards
 			Image i = c.getImage();
 			gc.drawImage(i, x + 5, y + 30);
 		}
 	}
 
-	//Draws player 1 hand
+	// Draws player 1 hand
 	public void drawPlayer1Hand() {
 		int tileSize = checkerboard.getTileSize();
 		int cardSizeX = checkerboard.getCardSizeX();
 		int cardSizeY = checkerboard.getCardSizeY();
-
 
 		for (Card c : player1Hand.getHand()) {
 			int handPosition = player1Hand.getHand().indexOf(c);
@@ -258,14 +253,13 @@ public class CheckerboardPane extends Pane {
 		}
 	}
 
-	//Draws the deck
+	// Draws the deck
 	public void drawDeck() {
 		int tileSize = checkerboard.getTileSize();
 		int cardSizeX = checkerboard.getCardSizeX();
 		int cardSizeY = checkerboard.getCardSizeY();
 
-
-		//Deck rectangle
+		// Deck rectangle
 		Rectangle deck = new Rectangle(10 + (tileSize * 8), 10 + (tileSize * 3), cardSizeX, cardSizeY);
 		deck.setFill(Color.AQUA);
 		deck.setStroke(Color.BLACK);
@@ -273,58 +267,55 @@ public class CheckerboardPane extends Pane {
 		deck.setArcHeight(20);
 		tileGroup.getChildren().add(deck);
 
-		//Deck text
+		// Deck text
 		Text deckText = new Text(10 + (tileSize * 8) + 10, 10 + (tileSize * 3) + 20, "Deck");
 		tileGroup.getChildren().add(deckText);
 
-		//Deck Image
+		// Deck Image
 		Image i = new Image("img/pileofshitpawn.png", 50, 50, true, true);
 		gc.drawImage(i, 10 + (tileSize * 8) + 5, 10 + (tileSize * 3) + 30);
 	}
 
-	//Draws the card that is hovered over by the mouse
+	// Draws the card that is hovered over by the mouse
 	public void drawEnlargedCard(Card c) {
 		int ID = c.getMovementTypeID();
 		Affiliation affiliation = c.getAffiliation();
 		ArrayList<String> cardDetails = Effect.getEffect(ID);
 
-
-		//Rectangle
-		Rectangle r = new Rectangle(10 + (checkerboard.getTileSize() * 8) + (3 * checkerboard.getCardSizeX()), 135, checkerboard.getCardSizeX() * 2, checkerboard.getCardSizeY() * 2 - 10);
-		if(affiliation == Affiliation.WHITE) {
+		// Rectangle
+		Rectangle r = new Rectangle(10 + (checkerboard.getTileSize() * 8) + (3 * checkerboard.getCardSizeX()), 135,
+				checkerboard.getCardSizeX() * 2, checkerboard.getCardSizeY() * 2 - 10);
+		if (affiliation == Affiliation.WHITE) {
 			r.setFill(Color.WHITE);
-		}
-		else {
+		} else {
 			r.setFill(Color.GREY);
 		}
 		r.setStroke(Color.BLACK);
-
 
 		// Rounded edges for units
 		r.setArcWidth(20);
 		r.setArcHeight(20);
 		tileGroup.getChildren().add(r);
 
-
-		//Title text
-		Text title = new Text(10 + (checkerboard.getTileSize() * 8) + (3 * checkerboard.getCardSizeX()) + 20, 175, cardDetails.get(0));
+		// Title text
+		Text title = new Text(10 + (checkerboard.getTileSize() * 8) + (3 * checkerboard.getCardSizeX()) + 20, 175,
+				cardDetails.get(0));
 		title.setUnderline(true);
 		title.setFont(new Font("Happy Monkey", 30));
 		tileGroup.getChildren().add(title);
 
-
-		//Image
+		// Image
 		Image image = new Image(cardDetails.get(2), 100, 100, true, true);
 		gc.drawImage(image, 10 + (checkerboard.getTileSize() * 8) + (3 * checkerboard.getCardSizeX()) + 10, 182);
 
-
-		//Description text
-		Text description = new Text((checkerboard.getTileSize() * 8) + (3 * checkerboard.getCardSizeX()) + 20, 300, cardDetails.get(1));
+		// Description text
+		Text description = new Text((checkerboard.getTileSize() * 8) + (3 * checkerboard.getCardSizeX()) + 20, 300,
+				cardDetails.get(1));
 		description.setWrappingWidth(100);
 		tileGroup.getChildren().add(description);
 	}
 
-	//Gets the card at the given x, y position
+	// Gets the card at the given x, y position
 	public Card getCardAt(int x, int y) {
 		Card c = null;
 		int tileSize = checkerboard.getTileSize();
@@ -332,37 +323,38 @@ public class CheckerboardPane extends Pane {
 		int rightMostCardRegion = leftMostCardRegion + (checkerboard.getCardSizeX() * 5);
 		int whiteCardRegionTop = 10;
 		int whiteCardRegionBottom = whiteCardRegionTop + checkerboard.getCardSizeY();
-		int blackCardRegionTop = 10 + (tileSize * 6);;
+		int blackCardRegionTop = 10 + (tileSize * 6);
+		;
 		int blackCardRegionBottom = blackCardRegionTop + checkerboard.getCardSizeY();
 
-
-		//If the x area is within the card area
-		if(x >= leftMostCardRegion && x < rightMostCardRegion) {
-			//If the y coordinate is in the white card region, retrieve and return the card at that position
-			if(y >= whiteCardRegionTop && y <= whiteCardRegionBottom) {
+		// If the x area is within the card area
+		if (x >= leftMostCardRegion && x < rightMostCardRegion) {
+			// If the y coordinate is in the white card region, retrieve and return the card
+			// at that position
+			if (y >= whiteCardRegionTop && y <= whiteCardRegionBottom) {
 				int cardIndex = (x - leftMostCardRegion) / checkerboard.getCardSizeX();
 				c = player0Hand.getHand().get(cardIndex);
 			}
-			//Else, if the y coordinate is in the black card region, retrieve and return the card at that position
-			else if(y >= blackCardRegionTop && y <= blackCardRegionBottom) {
+			// Else, if the y coordinate is in the black card region, retrieve and return
+			// the card at that position
+			else if (y >= blackCardRegionTop && y <= blackCardRegionBottom) {
 				int cardIndex = (x - leftMostCardRegion) / checkerboard.getCardSizeX();
 				c = player1Hand.getHand().get(cardIndex);
 			}
 		}
 
-
-		//Return null if no card is found
+		// Return null if no card is found
 		return c;
 	}
-	
+
 	public Hand getPlayer0Hand() {
 		return this.player0Hand;
 	}
-	
+
 	public Hand getPlayer1Hand() {
 		return this.player1Hand;
 	}
-	
+
 	public Deck getDeck() {
 		return this.deck;
 	}
@@ -372,12 +364,12 @@ public class CheckerboardPane extends Pane {
 		return this.checkerboard;
 	}
 
-	//Returns the set of units
+	// Returns the set of units
 	public Set<Unit> getUnits() {
 		return units;
 	}
 
-	//Returns the graphics context
+	// Returns the graphics context
 	public GraphicsContext getGraphicsContext() {
 		return gc;
 	}
